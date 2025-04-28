2a90cb61e00e8d285853283f2e7293911b92200a
minu2001 <alsdn3015@g.shingu.ac.kr>
2025-04-28 16:06:03 +0900

    second commit

---
+++ src/main/java/com/sg/ai/HelloController.java
@@ -0,0 +1,16 @@
+package com.sg.ai;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "안녕하세요 sbb page입니다";
+    }
+}
+	
---
+++ src/main/java/com/sg/ai/controller/RestController.java
@@ -0,0 +1,33 @@
+package com.sg.ai.controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.sg.ai.domain.User;
+
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+	@GetMapping("/jblog")
+	public User httpGet() {
+		User finduser = User.builder().id(1).username("ai").password("222").email("assemble@aa.com").build();
+		return finduser;
+	}
+	@PostMapping("/jblog")
+	//public String httpPost(@RequestBody User user)
+	public String httpPost() {
+		return "Put Request precessed";
+	}
+	@DeleteMapping("/jblog")
+	public String httpDelete(@RequestParam int id){
+		return "Delete Request processed"+ id;
+	}
+	@PutMapping("/jblog")
+	public String httpPut() {
+		return "Put Reqeust processed" ;
+	}
+}
+
