b3c2bd35cf1d11c9e4806c89a660cf3e4d067c04
minu2001 <alsdn3015@g.shingu.ac.kr>
2025-05-19 15:50:32 +0900

    새로운 질문을 저장하고 답변하기

--- src/main/java/com/sg/ai/question/QuestionController.java
+++ src/main/java/com/sg/ai/question/QuestionController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
@@ -30,6 +32,18 @@ public class QuestionController {
         return "question_detail";
     }
 
+    @GetMapping("/question/create")
+    public String qeustionCreate() {
+    	return "question_form";
+    }
+    
+    @PostMapping("/question/create")
+    public String questionCreatePost(@RequestParam(value="subject") String subject, 
+    									@RequestParam(value="content") String content) {
+        // TODO 질문을 저장한다.
+    	this.questionService.create(subject, content);
+        return "redirect:/question/list";
+    }
 	// 자바의 메소드 접근 제어
 	// public, private, protected,
 
