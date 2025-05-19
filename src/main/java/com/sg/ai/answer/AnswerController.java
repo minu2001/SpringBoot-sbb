2aba390d08a526a05df2662a24a54662bc8f4105
minu2001 <alsdn3015@g.shingu.ac.kr>
2025-05-19 12:05:16 +0900

    ;;;

---
+++ src/main/java/com/sg/ai/answer/AnswerController.java
@@ -0,0 +1,30 @@
+package com.sg.ai.answer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.sg.ai.question.Question;
+import com.sg.ai.question.QuestionService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/answer")
+public class AnswerController {
+	
+	private final QuestionService questionService;
+	private final AnswerService answerService;
+	
+	@PostMapping("/create/{id}")
+	public String createAnswer(Model model, @PathVariable("id") Integer id, 
+			@RequestParam(value="content") String Content) {
+		Question question = this.questionService.getQuestion(id);
+		this.answerService.create(question, Content); 
+		return String.format("redirect:/question/detail/%s",id);
+	}
+}
---
+++ src/main/java/com/sg/ai/question/QuestionController.java
@@ -0,0 +1,51 @@
+package com.sg.ai.question;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+
+	private final QuestionService questionService;
+
+	@GetMapping("/question/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionService.getList();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
+    
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+    	Question question = this.questionService.getQuestion(id);
+    	model.addAttribute("question", question);
+        return "question_detail";
+    }
+
+	// 자바의 메소드 접근 제어
+	// public, private, protected,
+
+	public void pubmathod() {
+		
+	}
+
+	private void primethod() {
+
+	}
+
+	protected void promethod() {
+
+	}
+
+	void defmethod() {
+
+	}
+}
